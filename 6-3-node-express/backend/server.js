8b4318a349b2d0e7bf3cbc66ca49abc57d7cecb8
MohForSupport <bykelver1@gmail.com>
2026-04-06 10:17:55 +0300

    6.3 lab

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,18 +1,25 @@
-// TODO 1: Import and Create express app instance
+import express from "express";
+const app = express();
 
-// TODO 1: Define server port
+const PORT = 3000;
 
+import cors from "cors";
+app.use(cors());
 
-// TODO 4: Import and Apply CORS middleware
+import morgan from "morgan";
+app.use(morgan("dev"));
 
+import { getRandomQuote } from "./quotes.js";
 
-// TODO 5: Import and apply Morgan middleware
+app.get("/", (req, res) => {
+  res.send("Quote API is running");
+});
 
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
-// TODO 6.1: Create root route "/"
-
-
-// TODO 6.2: Create "/api/quote" route
-
-
-// TODO 7: Start server using app.listen
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
